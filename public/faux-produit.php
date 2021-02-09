209c3d5ffc7f524efc73ea6a46ff28465cd944ae
Emmanuel Ravrat <RAVRAT@sio.laon>
2021-02-09 11:48:18 +0100

    code du faux produit deplace dans le controleur

--- public/faux-produit.php
+++ public/faux-produit.php
@@ -1,20 +1,4 @@
 <?php
-require_once '../src/lib/helper.php';
-
-require_once '../src/repository/productRepository.php';
-
-$product = getFakeProduct();
-
-ob_start();
-
-require_once '../templates/product/productCard.php';
-
-$output = ob_get_clean();
-
-$dataPage = [
-    'title'=> 'Telem - Faux produit',
-    'titlePage'=> 'Fiche d\'un faux produit',
-    'mainContent' => $output
-];
-layout($dataPage);
+require_once '../src/controller/productController.php';
+productFakeAction();
 
--- src/controller/productController.php
+++ src/controller/productController.php
@@ -1,9 +1,26 @@
 <?php
 //ici toutes les fonctions qui permettent d'agir sur les produits
 
+//inclusions des fichiers utiles aux contrôleurs
+require_once '../src/lib/helper.php';
+require_once '../src/repository/productRepository.php';
 /**
  * Récupère les données d'un faux produit et communique celles-ci aux templates utilisés
  */
 function productFakeAction(){
+    $product = getFakeProduct();
+    ob_start();
+
+    require_once '../templates/product/productCard.php';
+
+    $output = ob_get_clean();
+
+    $dataPage = [
+        'title'=> 'Telem - Faux produit',
+        'titlePage'=> 'Fiche d\'un faux produit',
+        'mainContent' => $output
+    ];
+
+    layout($dataPage);
 
 }
